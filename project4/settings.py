81ffcd58b166acc5e837ca68c836f38e35d00de5
Himaloy Mondal <himaloymondal100@gmail.com>
2021-05-03 17:04:26 +0530

    FIx Error In deployment

--- project4/settings.py
+++ project4/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
